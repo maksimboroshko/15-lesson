488a4515187f626938e6f99f4f08b9f84ea65e04
Maksim Boroshko <maksimboroshko211@gmail.com>
2025-01-08 17:32:40 +0300

    fixed

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -3,49 +3,50 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
-
 public class RegistrationTests {
-    String url =  "https://reqres.in/api/register";
-    String url23 =  "https://reqres.in/api/users/23";
-    String unknownUrl23 =  "https://reqres.in/api/unknown/23";
-    String token = "QpwL5tke4Pnpja7X4";
-    String email = "{\"email\": \"sydney@fife\"}";
-    String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
 
+    private static final String URL = "https://reqres.in/api/register";
+    private static final String URL_23 = "https://reqres.in/api/users/23";
+    private static final String UNKNOWN_URL_23 = "https://reqres.in/api/unknown/23";
+    private static final String TOKEN = "QpwL5tke4Pnpja7X4";
+    private static final String EMAIL = "{\"email\": \"sydney@fife\"}";
+    private static final String DATA = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
 
     @Test
-    void SuccessfulRegistrationTest() {
+    void successfulRegistrationTest() {
         given()
-                .body(data)
+                .body(DATA)
                 .contentType(ContentType.JSON)
                 .log().uri()
                 .when()
-                .post(url)
+                .post(URL)
                 .then()
                 .log().status()
                 .log().body()
                 .body("id", is(4))
-                .body("token", is(token));
+                .body("token", is(TOKEN));
     }
+
     @Test
-    void UnSuccessfulRegistrationTest() {
+    void unSuccessfulRegistrationTest() {
         given()
-                .body(email)
+                .body(EMAIL)
                 .contentType(ContentType.JSON)
                 .log().uri()
                 .when()
-                .post(url)
+                .post(URL)
                 .then()
                 .log().status()
                 .log().body()
-                .body("error" , is("Missing password"));
+                .body("error", is("Missing password"));
     }
+
     @Test
-    void UserNotFound415Test() {
+    void userNotFound415Test() {
         given()
                 .log().uri()
                 .when()
-                .post(url23)
+                .post(URL_23)
                 .then()
                 .log().status()
                 .log().body()
@@ -53,29 +54,29 @@ public class RegistrationTests {
     }
 
     @Test
-    void UserNotFoundTest() {
+    void userNotFoundTest() {
         given()
                 .log().uri()
                 .when()
-                .get(url23)
+                .get(URL_23)
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(404)
                 .body(is("{}"));
     }
+
     @Test
-    void ResourceNotFoundTest() {
+    void resourceNotFoundTest() {
         given()
                 .log().uri()
                 .when()
-                .get(unknownUrl23)
+                .get(UNKNOWN_URL_23)
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(404)
                 .body(is("{}"));
     }
-
-
 }
+
